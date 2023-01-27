ec417043761793146f8b44c94e90f8b512e1dad8
George Mavroeidis <gdmavroeidis@hotmail.com>
2023-01-27 00:07:46 -0500

    Replace rigidbody movement with transform

--- UnityWorkshopGGJ2023/Assets/Scripts/Player.cs
+++ UnityWorkshopGGJ2023/Assets/Scripts/Player.cs
@@ -1,28 +1,29 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D _Rigidbody2D;
-    public float _Speed;
+    [FormerlySerializedAs("_Speed")] public float _speed;
     public TextMeshProUGUI _textField;
 
     // FixedUpdate is called once per frame
     void FixedUpdate()
     {
         // Move left and set a barrier
-        if (Input.GetKey(KeyCode.A) && _Rigidbody2D.position.x >= -6)
+        // Notice that we want the bar to stay within a certain range, thus we only move it if it's above a certain x-position
+        if (Input.GetKey(KeyCode.A) && transform.position.x >= -6)
         {
-            _Rigidbody2D.MovePosition(_Rigidbody2D.position - new Vector2(1.0f, 0.0f) * Time.deltaTime * _Speed);
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
         }
         // Move right and set a barrier
-        else if (Input.GetKey(KeyCode.D) && _Rigidbody2D.position.x <= 6)
+        else if (Input.GetKey(KeyCode.D) && transform.position.x <= 6)
         {
-            _Rigidbody2D.MovePosition(_Rigidbody2D.position + new Vector2(1.0f, 0.0f) * Time.deltaTime * _Speed);
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * Time.deltaTime * _speed;
         }
     }
 
-    // Activate text field uppon call
+    // Activate text field upon call
     public void RevealTextField()
     {
         _textField.gameObject.SetActive(true);
